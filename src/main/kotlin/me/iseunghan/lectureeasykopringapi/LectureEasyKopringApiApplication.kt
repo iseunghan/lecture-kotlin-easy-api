38a2a1ac2acfe704aad280574c8f0345415cdb58
iseunghan <tmdgks301@naver.com>
2025-02-27 21:59:05 +0900

    kopring 프로젝트 기본 세팅

---
+++ src/main/kotlin/me/iseunghan/lectureeasykopringapi/LectureEasyKopringApiApplication.kt
@@ -0,0 +1,11 @@
+package me.iseunghan.lectureeasykopringapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LectureEasyKopringApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<LectureEasyKopringApiApplication>(*args)
+}
---
+++ src/test/kotlin/me/iseunghan/lectureeasykopringapi/LectureEasyKopringApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.iseunghan.lectureeasykopringapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LectureEasyKopringApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
